3cd626d179a24c50c7307dc7e090d644b4a6adb3
Cosmorius <d.dubber@st.hanze.nl>
2022-06-08 17:49:20 +0200

    Made a start on the other buttons

--- chess_application.py
+++ chess_application.py
@@ -1,9 +1,10 @@
+import chess
 from kivy.app import App
 from kivy.lang.builder import Builder
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-
+from kivy.uix.togglebutton import ToggleButton
 
 KV = """
 FloatLayout:
@@ -63,6 +64,8 @@ class ChessApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.board = None
+        self.help = False
+        self.memory_board = chess.Board()
 
     def build(self):
         Window.size = [800, 800]
@@ -80,20 +83,36 @@ class ChessApp(App):
                 self.root.ids['{}_{}'.format(col_char, row_num)] = square
             self.board.add_widget(board_row)
         self.reset_board()
+        # Add button to turn help on
+        help_button = ToggleButton(text='Turn on help')
+        help_button.bind(on_press=self.turn_on_help)
+        help_button.bind(on_release=self.turn_off_help)
+        self.board.add_widget(help_button)
+        # Add reset button
+        reset_button = Button(text='Reset')
+        reset_button.bind(on_press=self.reset_board)
+        self.board.add_widget(reset_button)
         return
 
-    def reset_board(self):
+    def reset_board(self, _=None):
         for id in self.root.ids:
             if id != 'chess_board':
                 square = self.root.ids[id]
                 if id in START_POS:
-                    col = square.background_color
-                    if col == [1, 1, 1, 1]:
+                    if id in [key for key in START_POS if 'Black' in START_POS[key]]:
                         square.background_normal = "pictures\\{}.png".format(START_POS[id])
                     else:
                         square.background_color = [1, 1, 1, 1]
                         square.background_normal = "pictures\\{}BackBlack.png".format(START_POS[id])
+                else:
+                    square.background_normal = ""
         return
+
+    def turn_on_help(self, _):
+        self.help = True
+
+    def turn_off_help(self, _):
+        self.help = False
 
     def get_color(self, i, j):
         is_light_square = (i + j) % 2 != 0
