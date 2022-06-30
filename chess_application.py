0c5e329f68a2c4ab952ec936808ef2a37f7c6808
Cosmorius <d.dubber@st.hanze.nl>
2022-06-30 06:26:13 +0200

    Made a demo and added test for ble

--- chess_application.py
+++ chess_application.py
@@ -1,3 +1,6 @@
+import asyncio
+import sys
+
 import chess
 from kivy.app import App
 from kivy.lang.builder import Builder
@@ -5,7 +8,9 @@ from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+import numpy as np
 from kivy.uix.togglebutton import ToggleButton
+from bleak import BleakClient
 
 KV = """
 FloatLayout:
@@ -13,6 +18,9 @@ FloatLayout:
         id: chess_board
         orientation: "vertical"
 """
+
+ADDRESS = "D0:3A:1C:27:B8:7D"
+WRITE_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
 
 CHAR_DIC = {
     0: 'A',
@@ -23,6 +31,13 @@ CHAR_DIC = {
     5: 'F',
     6: 'G',
     7: 'H'
+}
+
+demo_dict = {
+    0: 'A_1',
+    1: 'B_1',
+    2: 'A_2',
+    3: 'B_2'
 }
 
 START_POS = {
@@ -75,17 +90,18 @@ class ChessApp(App):
         self.help = False
         self.memory_board = chess.Board()
         self.num_of_rows = 2
+        self.board_positions = np.array([0, 0, 0, 0])
 
     def build(self):
         Window.size = [800, 800]
         return Builder.load_string(KV)
 
-    def on_start(self):
+    def on_star1t(self):
         self.board = self.root.ids.chess_board
         for i in range(self.num_of_rows):
             board_row = BoxLayout(orientation='horizontal')
             for j in range(self.num_of_rows):
-                row_num = self.num_of_rows - i
+                row_num = 8 - i
                 col_char = CHAR_DIC[j]
                 color_code, color = self.get_color(i, j)
                 square = Square(background_normal="", background_color=color_code, colour=color)
@@ -102,9 +118,6 @@ class ChessApp(App):
         reset_button = Button(text='Reset')
         reset_button.bind(on_press=self.reset_board)
         self.board.add_widget(reset_button)
-        # Add display
-        display_box = Label(text='Display')
-        self.board.add_widget(display_box)
         return
 
     def reset_board(self, _=None):
@@ -123,11 +136,42 @@ class ChessApp(App):
                         square.background_color = [0, 0, 0, 1]
         return
 
-    def turn_on_help(self, _):
-        self.help = True
+    def on_start(self):
+        self.board = self.root.ids.chess_board
+        for i in range(self.num_of_rows):
+            board_row = BoxLayout(orientation='horizontal')
+            for j in range(self.num_of_rows):
+                row_num = self.num_of_rows - i
+                col_char = CHAR_DIC[j]
+                color_code, color = self.get_color(i, j)
+                square = Square(background_normal="", background_color=color_code, colour=color)
+                board_row.add_widget(square)
+                self.root.ids['{}_{}'.format(col_char, row_num)] = square
+            self.board.add_widget(board_row)
+        # Add button to turn help on
+        help_button = ToggleButton(text='Turn on')
+        help_button.bind(on_press=self.turn_on)
+        help_button.bind(on_release=self.turn_off)
+        self.board.add_widget(help_button)
+        # Add reset button
+        reset_button = Button(text='Reset')
+        reset_button.bind(on_press=self.reset_board_demo)
+        self.board.add_widget(reset_button)
 
-    def turn_off_help(self, _):
-        self.help = False
+    def reset_board_demo(self, _):
+        for id in self.root.ids:
+            if id != 'chess_board':
+                square = self.root.ids[id]
+                square.background_normal = ""
+        return
+
+    def turn_on(self, _):
+        while True:
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(self.get_board_positions())
+
+    def turn_off(self, _):
+        sys.exit()
 
     def get_color(self, i, j):
         is_light_square = (i + j) % 2 != 0
@@ -135,6 +179,32 @@ class ChessApp(App):
             return [1, 1, 1, 1], 'White'
         else:
             return [0, 0, 0, 1], 'Black'
+
+    async def get_board_positions(self):
+        async with BleakClient(ADDRESS) as client:
+            model_number = await client.read_gatt_char(WRITE_UUID)
+            board_positions = np.array([int(pos) for pos in model_number.decode('utf-8')])
+            if np.array_equal(self.board_positions, board_positions):
+                self.board_positions = board_positions
+                self.update_board()
+
+    def update_board(self):
+        positves = [demo_dict[positive] for positive in np.where(self.board_positions == 1)]
+        for id in self.root.ids:
+            square = self.root.ids[id]
+            if id in positves:
+                if len(positves) == 1:
+                    if square.colour == "White":
+                        square.background_normal = "pictures\\Pawn.png"
+                    else:
+                        square.background_normal = "pictures\\PawnBackBlack.png"
+                elif not square.background_normal:
+                    if square.colour == "White":
+                        square.background_normal = "pictures\\PawnBlack.png"
+                    else:
+                        square.background_normal = "pictures\\PawnBlackBackBlack.png"
+            elif id != "chess_board":
+                square.background_normal = ''
 
 
 if __name__ == '__main__':
--- test_ble.py
+++ test_ble.py
@@ -1,10 +1,18 @@
 import asyncio
-from bleak import BleakScanner
+from bleak import BleakClient
+
+address = "D0:3A:1C:27:B8:7D"
+measurment_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
 
 
 async def main():
-    devices = await BleakScanner.discover()
-    for d in devices:
-        print(d)
+    async with BleakClient(address) as client:
+        model_number = await client.read_gatt_char(measurment_UUID)
+        board_positions = [int(pos) for pos in model_number.decode('utf-8')]
 
-asyncio.run(main())
+def tyout():
+    while True:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
+
+tyout()
